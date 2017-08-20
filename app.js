16fd8aef900a378ce34d5f4ce6736e0beee804b1
cale312 <zikisacalesogaxa@gmail.com>
2017-08-20 11:17:24 +0200

    exploring fs module

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const fs = require('fs');
+
+// sync style
+// var readMe = fs.readFileSync('./readme.txt', 'utf8');
+//
+// var newReadMe = fs.readFileSync('./newReadme.txt', 'utf8');
+
+// using callbacks
+fs.readFile('./readme.txt', 'utf8', function(err, data) {
+    fs.writeFile('./writeme.txt', data);
+});
